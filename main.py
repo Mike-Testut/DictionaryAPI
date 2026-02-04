955734bccbc02ad22678b1c024631e29d777f878
Mike-Testut <michael.testut@gmail.com>
2026-02-03 22:53:32 -0800

    Setup Flask and initial routes

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template("Home.html")
+
+@app.route('/api/v1/<word>')
+def api_route(word):
+    definition = "Definition Placeholder"
+    return { "word": word,
+             "definition": definition
+             }
+
+if __name__ == '__main__':
+    app.run(debug=True)
